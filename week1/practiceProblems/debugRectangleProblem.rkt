e8f1fd547728471fc4d1eb30d4b381c583920b91
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2025-10-09 00:08:17 +0100

    Complete Debug Rectangle Problem
    Debug the given code and fix the error(s).

---
+++ week1/practiceProblems/debugRectangleProblem.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname debugRectangleProblem) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;PROBLEM:
+
+;Uncomment the code below and fix the error(s).
+
+(rectangle 10 20 "solid" "red")
