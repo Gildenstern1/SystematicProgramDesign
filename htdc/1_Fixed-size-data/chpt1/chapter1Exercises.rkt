c5395ddf4431d40a918f7482067a9c8a3ec85dd4
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-10 12:06:26 +0000

    Complete exercise 10
    Exercise 10 from HtDP book

--- htdc/1_Fixed-size-data/chpt1/chapter1Exercises.rkt
+++ htdc/1_Fixed-size-data/chpt1/chapter1Exercises.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter1Exercises) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
+(require 2htdp/image)
 
 (define (cartesian x y)
   (sqrt (+ (* x x) (* y y))))
@@ -37,4 +37,12 @@
 
 (define (cvolume  x)
   (* (* x x) x))
+
+(define in ...)
+
+(cond [(string? in) (string-length in)]
+      [(image? in) (* (image-height in) (image-width in))]
+      [(number? in) (abs in)]
+      [(boolean=? in #true) 10]
+      [(boolean=? in #false) 20])
   
