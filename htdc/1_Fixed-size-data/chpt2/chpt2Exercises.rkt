53b9b5d44f5d78455c07f773bf088e81ebbc9c73
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-11 15:04:27 +0000

    Complete Chpt2 Exercise 16

--- htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
+++ htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chpt2Exercises) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
 
 ;; Exercise 11
 ;(define (distance-from-origin x y)
@@ -27,7 +28,11 @@
 ;(string-last "Hello") ;"o"
 
 ;; Exercise 15
-(define (==> sunny friday)
-  (or (boolean=? sunny #false)
-      (boolean=? friday #true)))
+;(define (==> sunny friday)
+;  (or (boolean=? sunny #false)
+;      (boolean=? friday #true)))
+
+;; Exercise 16
+(define (image-area img)
+  (* (image-width img)(image-height img)))
   
