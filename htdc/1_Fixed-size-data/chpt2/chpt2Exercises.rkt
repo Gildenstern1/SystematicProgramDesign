b04b43a2b22c29afca7c73cbbd4693e31483c8c9
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-11 14:26:39 +0000

    Complete Chpt2 Exercise 12

--- htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
+++ htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
@@ -2,7 +2,12 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chpt2Exercises) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-;; Exercise 1
+;; Exercise 11
 ;(define (distance-from-origin x y)
 ;   (sqrt (+ (sqr x)(sqr y))))
 ;(distance-from-origin 3 4)
+
+;; Exercise 12
+;(define (cvolume side)
+;  (* side side side))
+;(cvolume 3)
