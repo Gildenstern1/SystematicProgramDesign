8314f3430a108066e1f204206fe0ffee3559489d
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-11 14:21:54 +0000

    Complete Chpt 2 Exercise 11

---
+++ htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname chpt2Exercises) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;; Exercise 1
+;(define (distance-from-origin x y)
+;   (sqrt (+ (sqr x)(sqr y))))
+;(distance-from-origin 3 4)
