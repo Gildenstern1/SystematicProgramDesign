4015ea4061abd22ad4ffcd7cb0dabe42c41f1e98
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-23 14:46:58 +0000

    Complete Chpt2 Exercise 29

--- htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
+++ htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
@@ -72,28 +72,65 @@
 ;(string-delete "HelloWorld" 5) ;"Helloorld"
 
 ;; Eercise 27
-(define base-attendance 120)
-(define base-ticket-price 5.0)
-(define change-in-attendance 15)
-(define change-in-ticket-price 0.1)
-(define fixed-cost-to-owner 180)
-(define cost-per-attendee 0.04)
+;(define base-attendance 120)
+;(define base-ticket-price 5.0)
+;(define change-in-attendance 15)
+;(define change-in-ticket-price 0.1)
+;(define fixed-cost-to-owner 180)
+;(define cost-per-attendee 0.04)
+;
+;(define (attendees ticket-price)
+;  (- base-attendance (* (- ticket-price base-ticket-price) (/ change-in-attendance change-in-ticket-price))))
+;
+;(define (revenue ticket-price)
+;  (* ticket-price (attendees ticket-price)))
+;
+;(define (cost ticket-price)
+;  (+ fixed-cost-to-owner (* cost-per-attendee (attendees ticket-price))))
+;
+;(define (profit ticket-price)
+;  (- (revenue ticket-price)
+;     (cost ticket-price)))
+;
+;(define (profit price)
+;  (- (* (+ 120
+;           (* (/ 15 0.1)
+;              (- 5.0 price)))
+;        price)
+;     (+ 180
+;        (* 0.04
+;           (+ 120
+;              (* (/ 15 0.1)
+;                 (- 5.0 price)))))))
+;
+;(profit 1)
+;(profit 2)
+;(profit 3)
+;(profit 4)
+;(profit 5)
 
-(define (attendees ticket-price)
-  (- base-attendance (* (- ticket-price base-ticket-price) (/ change-in-attendance change-in-ticket-price))))
-
-(define (revenue ticket-price)
-  (* ticket-price (attendees ticket-price)))
-
-(define (cost ticket-price)
-  (+ fixed-cost-to-owner (* cost-per-attendee (attendees ticket-price))))
-
-(define (profit ticket-price)
-  (- (revenue ticket-price)
-     (cost ticket-price)))
-
-(profit 1)
-(profit 2)
-(profit 3)
-(profit 4)
-(profit 5)
+;; Exercise 29
+;(define base-attendance 120)
+;(define base-ticket-price 5.0)
+;(define change-in-attendance 15)
+;(define change-in-ticket-price 0.1)
+;(define cost-per-attendee 1.5)
+;
+;(define (attendees ticket-price)
+;  (- base-attendance (* (- ticket-price base-ticket-price) (/ change-in-attendance change-in-ticket-price))))
+;
+;(define (revenue ticket-price)
+;  (* ticket-price (attendees ticket-price)))
+;
+;(define (cost ticket-price)
+;  (* cost-per-attendee (attendees ticket-price)))
+;
+;(define (profit ticket-price)
+;  (- (revenue ticket-price)
+;     (cost ticket-price)))
+;
+;(profit 1)
+;(profit 2)
+;(profit 3)
+;(profit 4)
+;(profit 5)
