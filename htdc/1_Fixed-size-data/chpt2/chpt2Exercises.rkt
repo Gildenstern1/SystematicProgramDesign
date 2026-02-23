cad138fe6a6c0cfa2582193ce153b2fdf757873a
John Carthy <145382179+Gildenstern1@users.noreply.github.com>
2026-02-23 14:41:09 +0000

    Complete Chpt 2 Exercise 27

--- htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
+++ htdc/1_Fixed-size-data/chpt2/chpt2Exercises.rkt
@@ -70,3 +70,24 @@
 ;                 (substring str (+ num 1))))
 
 ;(string-delete "HelloWorld" 5) ;"Helloorld"
+
+;; Eercise 27
+(define base-attendance 120)
+(define base-ticket-price 5.0)
+(define change-in-attendance 15)
+(define change-in-ticket-price 0.1)
+(define fixed-cost-to-owner 180)
+(define cost-per-attendee 0.04)
+
+(define (attendees ticket-price)
+  (- base-attendance (* (- ticket-price base-ticket-price) (/ change-in-attendance change-in-ticket-price))))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (+ fixed-cost-to-owner (* cost-per-attendee (attendees ticket-price))))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
